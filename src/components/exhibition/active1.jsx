61eb4cb4f464073b2d75171540088445a853dc2e
prosper edafe <mamusedafe19@gmail.com>
2023-01-25 17:40:45 +0100

    active pages created

---
+++ src/components/exhibition/active1.jsx
@@ -0,0 +1,42 @@
+import arrowDown from "../../assets/Drop-down chevron.png";
+import "./active.css";
+import { useState } from "react";
+
+function Active1() {
+  const [show, setShow] = useState(false);
+  return (
+    <div className="active-exhibition-tab">
+      <img src="" alt="" />
+      <p className="about">
+        “Abandoned Dust Bowl Home”
+        <br /> Gelatin silver print
+        <br /> about 1935 - 1940
+      </p>
+      <div onClick={() => setShow(!show)} className="arrow-down">
+        <h2>BIOGRAPHY</h2>
+        <img className={show ? "rotate" : ""} src={arrowDown} alt="arrow" />
+      </div>
+      {show && (
+        <p className="description">
+          Dorothea Lange (May 26, 1895 – October 11, 1965) was an influential
+          American documentary photographer and photojournalist, best known for
+          her Depression-era work for the Farm Security Administration (FSA).
+          Lange's photographs humanized the consequences of the Great Depression
+          and influenced the development of documentary photography. Born of
+          second generation German immigrants on May 26, 1895, at 1041
+          Bloomfield Street, Hoboken, New Jersey,[1][2] Dorothea Lange was named
+          Dorothea Margaretta Nutzhorn at birth. She dropped her middle name and
+          assumed her mother's maiden name after her father abandoned the family
+          when she was 12 years old, one of two traumatic incidents early in her
+          life. The other was her contraction of polio at age seven which left
+          her with a weakened right leg and a permanent limp.[1][2] "It formed
+          me, guided me, instructed me, helped me and humiliated me," Lange once
+          said of her altered gait. "I've never gotten over it, and I am aware
+          of the force and power of it."[3]
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default Active1;
--- src/pages/exhibition/exhibition.jsx
+++ src/pages/exhibition/exhibition.jsx
@@ -1,4 +1,10 @@
+import "./exhibition.css";
+import { useState } from "react";
+import Active1 from "../../components/exhibition/active1";
+import Active2 from "../../components/exhibition/active2";
+
 const Exhibition = () => {
+  const [activeTab, setActiveTab] = useState("active1");
   return (
     <section className="exhibition">
       <h3>RETROSPECTIVE</h3>
@@ -8,6 +14,34 @@ const Exhibition = () => {
       </h1>
       <time>OCTOBER 15 - MARCH 18</time>
       <h3>FLOOR 5</h3>
+
+      <div className="active-exhibition">
+        <button
+          className={activeTab === "active1" ? "current" : ""}
+          onClick={() => setActiveTab("active1")}
+        ></button>
+        <button
+          className={activeTab === "active2" ? "current" : ""}
+          onClick={() => setActiveTab("active2")}
+        ></button>
+        <button
+          className={activeTab === "active3" ? "current" : ""}
+          onClick={() => setActiveTab("active3")}
+        ></button>
+        <button
+          className={activeTab === "active4" ? "current" : ""}
+          onClick={() => setActiveTab("active4")}
+        ></button>
+        <button
+          className={activeTab === "active5" ? "current" : ""}
+          onClick={() => setActiveTab("active5")}
+        ></button>
+      </div>
+      <section className="active-state">
+        {activeTab === "active1" && <Active1 />}
+        {activeTab === "active2" && <Active2 />}
+        {/* {activeTab === "counci3" && <Council />} */}
+      </section>
     </section>
   );
 };
